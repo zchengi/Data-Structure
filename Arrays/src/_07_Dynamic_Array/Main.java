357b4a015fe97cdfef1c13a020efdbd57b8199fa
cheng <792702352@qq.com>
2018-05-09 13:14:56 +0800

    动态数组实现

---
+++ Arrays/src/_07_Dynamic_Array/Main.java
@@ -0,0 +1,46 @@
+package _07_Dynamic_Array;
+
+/**
+ * @author cheng
+ *         2018/5/9 12:08
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Array<String> arr = new Array<>();
+
+        int size = 10;
+        for (int i = 0; i < size; i++) {
+            arr.addLast(String.valueOf(i+"zz"));
+        }
+        System.out.println(arr);
+
+        arr.add(1, String.valueOf(100));
+        System.out.println(arr);
+
+        arr.addFirst(String.valueOf(-1));
+        System.out.println(arr);
+
+        arr.addLast(String.valueOf(233));
+        System.out.println(arr.find(String.valueOf(233)));
+        System.out.println(arr.removeElement(String.valueOf(233)));
+        System.out.println(arr.removeElement(String.valueOf(233)));
+
+        arr.addLast(String.valueOf(233));
+        arr.addLast(String.valueOf(233));
+        arr.addLast(String.valueOf(233));
+        System.out.println(arr.findAll(String.valueOf(233)));
+        System.out.println(arr.removeAllElement(String.valueOf(233)));
+        System.out.println(arr.removeAllElement(String.valueOf(233)));
+
+        System.out.println(arr);
+        System.out.println(arr.removeFirst());
+        System.out.println(arr.removeLast());
+        System.out.println(arr);
+
+        while (!arr.isEmpty()) {
+            arr.removeFirst();
+        }
+        System.out.println(arr);
+    }
+}
