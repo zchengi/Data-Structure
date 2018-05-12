3a130b457fbd74fa1043d201f3ea7f86f32cc8bb
cheng <792702352@qq.com>
2018-05-12 15:15:36 +0800

    add removeElement(E e) to LinkedList

--- My-Data-Structures/src/linkedlists/LinkedList.java
+++ My-Data-Structures/src/linkedlists/LinkedList.java
@@ -174,6 +174,29 @@ public class LinkedList<E> {
     }
 
     /**
+     * 从链表中删除元素 e
+     *
+     * @param e
+     */
+    public void removeElement(E e) {
+
+        Node prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.e.equals(e)) {
+                break;
+            }
+            prev = prev.next;
+        }
+
+        if (prev.next != null) {
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            size--;
+        }
+    }
+
+    /**
      * 查找链表中是否有元素 e
      *
      * @param e
